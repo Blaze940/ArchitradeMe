f1417f32853989287c409fd558d2693a8f35c533
Mouss <mdiarra25@myges.fr>
2022-11-16 22:02:34 +0100

    Initial commit

---
+++ src/test/java/com/esgi/architrademe/ArchitradeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.architrademe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitradeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
